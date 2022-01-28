26238baccc1fbb0d3b8a7e3134906f796f86eb2d
Yuchao Dai <icyblade.py@gmail.com>
2022-01-28 23:35:05 +0800

    Minor fix

--- Modules/Others/Core.lua
+++ Modules/Others/Core.lua
@@ -1,3 +1,13 @@
 local _, Engine = ...
 
 Engine.Modules.Utils = {}
+
+Engine.Modules.Utils.Core =
+    Engine.AceEvent:RegisterEvent(
+    'LOADING_SCREEN_DISABLED',
+    function(...)
+        ConsoleExec('ffxGlow', 0)
+        ConsoleExec('weatherDensity', 1)
+        ConsoleExec('cameraDistanceMaxZoomFactor', 3.4)
+    end
+)
--- Modules/Others/ProfanityFilter.lua
+++ Modules/Others/ProfanityFilter.lua
@@ -1,10 +1,11 @@
 local _, Engine = ...
 
-Engine.Modules.Utils.AutoDismount =
+Engine.Modules.Utils.ProfanityFilter =
     Engine.AceEvent:RegisterEvent(
     'LOADING_SCREEN_DISABLED',
     function(...)
         ConsoleExec('portal TW')
 	    ConsoleExec('profanityFilter 0')
+        ConsoleExec('overrideArchive 0')
     end
 )
